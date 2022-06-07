aa6f1c25e6ef09307770cb7cb3d45d6f00e7837e
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-07 11:46:23 +0200

    Exo2, active

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
+let exos = document.querySelectorAll(".exo")
+
+// Exo1
+let exo1 = document.getElementById("exo1")
 let titre = document.querySelector("h2")
 titre.innerText = "le titre modifié"
 titre.style.color = "red"
 titre.style.backgroundColor = "#007BFF"
 titre.classList.add("bg-success")
+
+// Exo2
+let exo2 = document.getElementById("exo2")
+// Active
+exos.forEach(element => {
+    element.addEventListener("click", ()=> {
+        let x = document.querySelector(".active")
+        x.setAttribute("class","")
+        element.classList.add("active")
+    })
+});
