ebcff49046ec3473a806b8372c156a8918582cf1
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-08 09:45:17 +0200

    Exo4 : done

--- public/js/main.js
+++ public/js/main.js
@@ -57,3 +57,27 @@ input.addEventListener("keypress", (event) => {
         Exo3()
     }
 })
+
+
+// Exo4 
+let btn4 = document.querySelectorAll("button")[1]
+let dispClick = document.querySelectorAll("span")[1]
+let click = 0
+
+let rectangle1 = document.querySelector(".rectangle1")
+let rectangle2 = document.querySelector(".rectangle2")
+let rouge1 = rectangle1.childNodes[1]
+let rouge2 = rectangle2.childNodes[1]
+
+btn4.addEventListener("click", () => {
+    if (rouge1.classList.contains("red")) {
+        rouge2.classList.add("red")
+        rouge1.classList.remove("red")
+    } else if (rouge2.classList.contains("red")) {
+        rouge1.classList.add("red")
+        rouge2.classList.remove("red")
+    }
+    // Nombre de clicks
+    click += 1;
+    dispClick.innerHTML = click;
+})
